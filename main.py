965375f1d57ca388eecf561fc6028d1e892240d0
Skalyne <theo.kratz.t@gmail.com>
2025-01-19 19:38:27 +0100

    refactor github api

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from pr_analysis import DiffAnalysis
-from pr_data import PullRequestData
+from github_api import GithubAPI
 
 pr_url = "https://api.github.com/repos/Skalyne/PR_AI_Analysis/pulls/3"
 
-pr_data = PullRequestData(pr_url)
-diff = pr_data.get_pr_diff()
+pr_data = GithubAPI(pr_url)
+diff = pr_data.diff_data
 
 analysis = DiffAnalysis(diff)
 generate = analysis.generate_response()
 
 for item in generate:
-    comment = pr_data.make_comments(item)
+    comment = pr_data.send_pr_review(item)
