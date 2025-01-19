d2ceea7b5b335992ede61778b702b07eb2cbea67
Skalyne <theo.kratz.t@gmail.com>
2025-01-19 10:47:37 +0100

    add data

--- pr_data.py
+++ pr_data.py
@@ -18,3 +18,18 @@ class PullRequestData:
         response = requests.get(diff_url, headers=GITHUB_HEADERS)
         response.raise_for_status()
         return response.text
+    
+    def make_comments(self, json_body):
+        comments_url = str(self.pr_data["review_comments_url"]).replace("/comments", "/reviews")
+        json_body["commit_id"] = self.pr_data["merge_commit_sha"]
+
+        resposne = requests.post(comments_url, headers=GITHUB_HEADERS, json=json_body)
+        resposne.raise_for_status()
+        return resposne.text
+
+   
+# if __name__ == "__main__":
+#     print(GITHUB_HEADERS)
+#     pr_url = "https://api.github.com/repos/Skalyne/PR_AI_Analysis/pulls/1"
+#     data = PullRequestData(pr_url)
+#     data.get_pr_diff()
